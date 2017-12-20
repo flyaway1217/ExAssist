0b2bca72ceafc53291fb77d4b9beeb41ad21f1c6
flyaway1217 <flyaway1217@gmail.com>
2017-12-19 20:43:58 -0700

    add exception test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -7,13 +7,16 @@
 # Python release: 3.6.0
 #
 # Date: 2017-12-18 21:04:39
-# Last modified: 2017-12-18 22:01:55
+# Last modified: 2017-12-19 20:42:59
 
 """
 Test for Assist
 """
-import ExAssist as EA
 import configparser
+
+import pytest
+
+import ExAssist as EA
 
 
 class TestEA:
@@ -25,8 +28,16 @@ class TestEA:
                 interpolation=configparser.ExtendedInterpolation())
         config.read('tests/config.ini', encoding='utf8')
         assist.config = config
+        assist.comments = 'This is a test comment'
         assist.start()
 
     def test_getting(self):
         assist = EA.getAssist('Test')
         assert assist.ex_dir == 'tests/Experiments/'
+
+    def test_start_exception(self):
+        assist = EA.getAssist('Test')
+        s = 'Assist has been locked,  can not add more comments'
+        with pytest.raises(Exception) as excinfo:
+            assist.comments = 'This is a test.'
+        assert str(excinfo.value) == s
