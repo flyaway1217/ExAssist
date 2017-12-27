276c2e5584ff6032c7b9f2797db8d33a48f5cd35
flyaway1217 <flyaway1217@gmail.com>
2017-12-26 19:54:15 -0700

    plot the loss curve

--- ExAssist/templates/js/script.js
+++ ExAssist/templates/js/script.js
@@ -1,3 +1,29 @@
-d3.json('run.json', function(error, data){
+d3.json('./info.json', function(error, data){
     console.log(data);
+    var ctx = document.getElementById('loss_chart').getContext('2d');
+    var labels = [];
+    var y = [];
+    var i;
+    for(i=0;i < data.length;++i){
+        labels.push(i.toString());
+        y.push(data[i].loss);
+    }
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+
+        // The data for our dataset
+        data: {
+            labels: labels, 
+            datasets: [{
+                label: "Loss",
+                borderColor: 'rgb(255, 99, 132)',
+                data: y,
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
 });
